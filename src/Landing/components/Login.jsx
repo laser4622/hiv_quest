ed2e5c75cacd8209624a9da52d7a430f29c3bd58
feddot2517@gmail.com <8D2g1101>
2020-06-25 00:36:49 +0600

    Login

--- src/Landing/components/Login.jsx
+++ src/Landing/components/Login.jsx
@@ -2,38 +2,55 @@ import React, {useState} from 'react';
 import InputMask from "react-input-mask";
 import {gameLogin, getStatus} from "../../api";
 import appStore from "../../store";
+import './Login.css'
 
-const Login = () => {
-    const [value, setValue] = useState();
+const Login = (props) => {
+    const [value, setValue] = useState('');
+    const [code, setCode] = useState('');
 
     const onClick = async () => {
         await appStore.login(value);
         await appStore.updateStatus()
-        // console.log(token)
-        // await getStatus(token)
+
+        props.history.push('/game')
     };
 
     return (
-        <div className="Game">
+        <div className="Login"
+                     >
+                        <span>Войти</span>
+                        <form onSubmit={()=>console.log('qwe')}>
+                         <InputMask
+                             mask="+7\ (999) 999-99-99"
+                             placeholder="Номер телефона"
+                             type="tel"
+                             name="phone"
+                             pattern="[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}"
+                             minLength="18"
+                             required
+                             onChange={(e)=>{setValue(e.target.value)}}
+                         >
+                             {(inputProps) => <input id="login-tel" required {...inputProps} type="tel"/>}
+                         </InputMask>
 
-            <InputMask
-                mask="+7\ (999) 999-99-99"
-                placeholder="Номер телефона"
-                type="tel"
-                name="phone"
-                pattern="[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}"
-                minLength="18"
-                required
-                onChange={(e)=>{setValue(e.target.value)}}
-            >
-                {/*{(inputProps) => <input id="tel" required {...inputProps} type="tel"/>}*/}
-            </InputMask>
+                         <InputMask
+                             mask="999-999"
+                             placeholder="Код из смс"
+                             type="code"
+                             pattern="{3}[\-]\d{3}"
+                             minLength="6"
+                             required
+                             onChange={(e)=>{setValue(e.target.value)}}
+                         >
+                             {(inputProps) => <input id="login-code" required {...inputProps}/>}
+                         </InputMask>
 
+                         </form>
 
-            <button onClick={onClick}>click</button>
+                         <button>click</button>
 
-        </div>
-    )
-};
+                     </div>
+                 )
+             };
 
-export default Login;
+             export default Login;
