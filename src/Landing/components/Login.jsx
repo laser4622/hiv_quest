6a208f596dd34034aae2af5fd3834281e9db4909
feddot2517@gmail.com <8D2g1101>
2020-07-01 00:52:56 +0600

    111

--- src/Landing/components/Login.jsx
+++ src/Landing/components/Login.jsx
@@ -66,7 +66,7 @@ const Login = (props) => {
                 <SubmitButton text="Войти"/>
                 <div onClick={() => setHintVisible(!hintVisible)} className="Login-help">Помощь</div>
                 {hintVisible &&
-                <div className="Login-help_hint">Если вам не пришло смс-сообщение с кодом, то свяжитесь с нами. Почта: feddot2517@gmail.com. <br/>Телефон: <a href="tel:+79994598334">+7 999-459-83-34</a></div>}
+                <div className="Login-help_hint">Если вам не пришло смс-сообщение с кодом, то свяжитесь с нами. Сообщения в группу: <a href="https://vk.com/goquest.online ">VK</a><br/>Телефон: <a href="tel:+79994598334">+7 999-459-83-34</a></div>}
             </form>
 
         </div>
